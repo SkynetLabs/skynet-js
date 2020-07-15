85a04ef7222ee32ea870ffc6fc4f03c0dae7cc5e
Karol Wypchlo <kwypchlo@gmail.com>
2020-07-15 17:17:44 +0200

    ensure file object consistency in firefox

--- src/upload.js
+++ src/upload.js
@@ -4,7 +4,7 @@ import parse from "url-parse";
 export async function upload(portalUrl, file, options = {}) {
   const formData = new FormData();
 
-  formData.append("file", file);
+  formData.append("file", ensureFileObjectConsistency(file));
 
   const parsed = parse(portalUrl);
 
@@ -29,7 +29,7 @@ export async function uploadDirectory(portalUrl, directory, filename, options = 
   const formData = new FormData();
 
   Object.entries(directory).forEach(([path, file]) => {
-    formData.append("files[]", file, path);
+    formData.append("files[]", ensureFileObjectConsistency(file), path);
   });
 
   const parsed = parse(portalUrl);
@@ -50,4 +50,15 @@ export async function uploadDirectory(portalUrl, directory, filename, options = 
   );
 
   return data;
+}
+
+/**
+ * Sometimes file object might have had the type property defined menually with Object.defineProperty
+ * and some browsers (namely firefox) can have problems reading it after the file has been appended
+ * to form data. To overcome this, we recreate the file object using native File constructor with
+ * a type defined as a constructor argument.
+ * Related issue: https://github.com/NebulousLabs/skynet-webportal/issues/290
+ */
+export function ensureFileObjectConsistency(file) {
+  return new File([file], file.name, { type: file.type });
 }
