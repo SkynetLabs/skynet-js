9b1017c14141f9ac4ebc6cf471df35e2b38331be
Marcin S <scatman@bu.edu>
2020-08-28 16:56:42 +0200

    Remove administrative API

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 export { SkynetClient } from "./client.js";
 
 // Get the following files to run or the client's methods won't be defined.
-export {} from "./blocklist.js";
-export {} from "./convert.js";
 export {} from "./download.js";
 export {} from "./encryption.js";
-export {} from "./list.js";
-export {} from "./pin.js";
-export {} from "./portals.js";
-export {} from "./stats.js";
 export {} from "./upload.js";
 
 export {
--- src/index.test.js
+++ src/index.test.js
@@ -4,13 +4,6 @@ describe("SkynetClient", () => {
   it("should contain all api methods", () => {
     const client = new SkynetClient();
 
-    // Blocklist
-    expect(client).toHaveProperty("getBlocklist");
-    expect(client).toHaveProperty("updateBlocklist");
-
-    // Convert
-    expect(client).toHaveProperty("convert");
-
     // Download
     expect(client).toHaveProperty("downloadFile");
     expect(client).toHaveProperty("downloadFileHns");
@@ -28,20 +21,6 @@ describe("SkynetClient", () => {
     expect(client).toHaveProperty("getSkykeyById");
     expect(client).toHaveProperty("getSkykeyByName");
     expect(client).toHaveProperty("getSkykeys");
-
-    // List
-    expect(client).toHaveProperty("listFiles");
-
-    // Pin
-    expect(client).toHaveProperty("pin");
-    expect(client).toHaveProperty("unpin");
-
-    // Portals
-    expect(client).toHaveProperty("getPortals");
-    expect(client).toHaveProperty("updatePortals");
-
-    // Stats
-    expect(client).toHaveProperty("getStats");
 
     // Upload
     expect(client).toHaveProperty("uploadFile");
