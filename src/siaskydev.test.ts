696d93e4fc0181e5cad2d626837d48b11684a323
PJ <peterjan.brone@gmail.com>
2020-10-15 19:55:03 +0200

    WIP commit

--- src/siaskydev.test.ts
+++ src/siaskydev.test.ts
@@ -8,10 +8,12 @@ const filename = "hello.txt";
 const fileID = new FileID(appID, FileType.PublicUnencrypted, filename);
 
 // skip - used for debugging purposes
-describe.skip("siasky.dev end to end", () => {
+describe("siasky.dev end to end", () => {
   it.only("should work", async () => {
     const user = new User("john.doe@gmail.com", "test1234");
-    const file = new File(["thisistext"], filename, { type: "text/plain" });
-    await client.setFile(user, fileID, new SkyFile(file));
+    // const file = new File(["thisistext"], filename, { type: "text/plain" });
+    // await client.setFile(user, fileID, new SkyFile(file));
+    const file = await client.getFile(user, fileID);
+    console.log(file.file)
   });
 });
