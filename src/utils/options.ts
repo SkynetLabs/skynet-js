dfa00febb5a4a3bafe87891952375a3b5c5c7a33
Marcin S <marcin@bytedude.com>
2021-06-01 12:53:31 -0500

    Fix mySky.setEntryData
    NOTE: This is a temporary fix, as the SkyDB caching will remove the need for the
    setEntryData options to contain getEntry options, and we can change the option
    type from CustomSetJSONOptions to CustomSetEntryOptions at that time.

--- src/utils/options.ts
+++ src/utils/options.ts
@@ -21,13 +21,19 @@ export const defaultBaseOptions = {
  * @param opts - The given options.
  * @param model - The model options.
  * @returns - The extracted custom options.
+ * @throws - If the given opts don't contain all properties of the model.
  */
 export function extractOptions<T extends Record<string, unknown>>(opts: Record<string, unknown>, model: T): T {
   const result: Record<string, unknown> = {};
   for (const property in model) {
-    if (Object.prototype.hasOwnProperty.call(model, property)) {
-      result[property] = opts[property];
+    if (!Object.prototype.hasOwnProperty.call(model, property)) {
+      continue;
     }
+    // Throw if the given options don't contain the model's property.
+    if (!Object.prototype.hasOwnProperty.call(opts, property)) {
+      throw new Error(`Property '${property}' not found`);
+    }
+    result[property] = opts[property];
   }
 
   return result as T;
