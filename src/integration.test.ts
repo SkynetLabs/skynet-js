78c1c5310c4dfdcbab785598462f6e38a2ce8900
Marcin S <marcin@bytedude.com>
2021-06-28 13:51:33 -0500

    Add back tests that were removed in merge

--- src/integration.test.ts
+++ src/integration.test.ts
@@ -95,6 +95,28 @@ describe(`Integration test for portal ${portal}`, () => {
     });
   });
 
+  describe("Encrypted File API integration tests", () => {
+    const userID = "4dfb9ce035e4e44711c1bb0a0901ce3adc2a928b122ee7b45df6ac47548646b0";
+    // Path seed for "test.hns/encrypted".
+    const pathSeed = "fe2c5148646532a442dd117efab3ff2a190336da506e363f80fb949513dab811";
+
+    it("Should get existing encrypted JSON", async () => {
+      const expectedJson = { message: "foo" };
+
+      const { data } = await client.file.getJSONEncrypted(userID, pathSeed);
+
+      expect(data).toEqual(expectedJson);
+    });
+
+    it("Should return null for inexistant encrypted JSON", async () => {
+      const pathSeed = "a".repeat(64);
+
+      const { data } = await client.file.getJSONEncrypted(userID, pathSeed);
+
+      expect(data).toBeNull();
+    });
+  });
+
   describe("SkyDB end to end integration tests", () => {
     it("Should get existing SkyDB data", async () => {
       const publicKey = "89e5147864297b80f5ddf29711ba8c093e724213b0dcbefbc3860cc6d598cc35";
