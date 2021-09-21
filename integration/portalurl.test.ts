b718a715955545876916831860ca585e444b42fa
Marcin S <marcin@bytedude.com>
2021-09-21 17:03:34 -0500

    Fix test failure

--- integration/portalurl.test.ts
+++ integration/portalurl.test.ts
@@ -1,7 +1,8 @@
-import {} from ".";
+import { portal } from ".";
 
 describe("toEqualPortalUrl", () => {
   it("Subdomained portal servers should equal main portal", () => {
+    expect(portal).toEqualPortalUrl(portal);
     expect("https://us-ny-2.siasky.net").toEqualPortalUrl("https://siasky.net");
   });
 });
