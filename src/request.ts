663e344ef5dc860e5638df32584d442ed80f02e5
Marcin S <marcin@bytedude.com>
2021-12-10 13:52:12 -0600

    Address review comments

--- src/request.ts
+++ src/request.ts
@@ -14,41 +14,46 @@ export class ExecuteRequestError extends Error {
     // Required for `instanceof` to work.
     Object.setPrototypeOf(this, ExecuteRequestError.prototype);
   }
-}
 
-/**
- * Gets the full, descriptive error response returned from skyd on the portal.
- *
- * @param err - The Axios error.
- * @returns - A new error if the error response is malformed, or the skyd error message otherwise.
- */
-export function getPortalErrResponse(err: AxiosError): ExecuteRequestError {
-  /* istanbul ignore next */
-  if (!err.response) {
-    return new ExecuteRequestError(`Error repsonse did not contain expected field 'response'.`, err, null, null);
-  }
-  /* istanbul ignore next */
-  if (!err.response.status) {
-    return new ExecuteRequestError(`Error response did not contain expected field 'response.status'.`, err, null, null);
-  }
+  /**
+   * Gets the full, descriptive error response returned from skyd on the portal.
+   *
+   * @param err - The Axios error.
+   * @returns - A new error if the error response is malformed, or the skyd error message otherwise.
+   */
+  static From(err: AxiosError): ExecuteRequestError {
+    /* istanbul ignore next */
+    if (!err.response) {
+      return new ExecuteRequestError(`Error repsonse did not contain expected field 'response'.`, err, null, null);
+    }
+    /* istanbul ignore next */
+    if (!err.response.status) {
+      return new ExecuteRequestError(
+        `Error response did not contain expected field 'response.status'.`,
+        err,
+        null,
+        null
+      );
+    }
 
-  const status = err.response.status;
+    const status = err.response.status;
 
-  // If we don't get an error message from skyd, just return the status code.
-  /* istanbul ignore next */
-  if (!err.response.data) {
-    return new ExecuteRequestError(`Request failed with status code ${status}.`, err, status, null);
-  }
-  /* istanbul ignore next */
-  if (!err.response.data.message) {
-    return new ExecuteRequestError(`Request failed with status code ${status}.`, err, status, null);
-  }
+    // If we don't get an error message from skyd, just return the status code.
+    /* istanbul ignore next */
+    if (!err.response.data) {
+      return new ExecuteRequestError(`Request failed with status code ${status}.`, err, status, null);
+    }
+    /* istanbul ignore next */
+    if (!err.response.data.message) {
+      return new ExecuteRequestError(`Request failed with status code ${status}.`, err, status, null);
+    }
 
-  // Return the error message from skyd. Pass along the original Axios error.
-  return new ExecuteRequestError(
-    `Request failed with status code ${err.response.status}: ${err.response.data.message}`,
-    err,
-    status,
-    err.response.data.message
-  );
+    // Return the error message from skyd. Pass along the original Axios error.
+    return new ExecuteRequestError(
+      `Request failed with status code ${err.response.status}: ${err.response.data.message}`,
+      err,
+      status,
+      err.response.data.message
+    );
+  }
 }
