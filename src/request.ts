746fe294576f88e1a5dc6d33e277978875b6195f
Karol Wypchlo <kwypchlo@gmail.com>
2022-09-12 13:03:37 +0200

    fix change breaking tests

--- src/request.ts
+++ src/request.ts
@@ -183,11 +183,11 @@ export class ExecuteRequestError<T = any, D = any> extends Error implements Axio
     // If we don't get an error message from skyd, just return the status code.
     /* istanbul ignore next */
     if (!err.response.data) {
-      return new ExecuteRequestError(`Request failed with status code ${status}.`, err, status, null);
+      return new ExecuteRequestError(`Request failed with status code ${status}`, err, status, null);
     }
     /* istanbul ignore next */
     if (!err.response.data.message) {
-      return new ExecuteRequestError(`Request failed with status code ${status}.`, err, status, null);
+      return new ExecuteRequestError(`Request failed with status code ${status}`, err, status, null);
     }
 
     // Return the error message from skyd. Pass along the original Axios error.
